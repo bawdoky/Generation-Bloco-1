51320dcd08e847cfac5ea349f7bf3206e2936341
Lucas Oi <lucas.oi-@hotmail.com>
2021-12-14 23:41:00 -0300

    Lista 5 POO

---
+++ java/Bloco 1/Listas/Lista5POO/TesteCliente.java
@@ -0,0 +1,13 @@
+package POO;
+
+public class TesteCliente 
+{
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		Clientes cliente1 = new Clientes("Lucas","23","55556666","lucas.oi@gmail.com","SãoPaulo-SP");
+		//System.out.println(cliente1.getInfo());
+		cliente1.imprimaInfo();
+	}
+}
