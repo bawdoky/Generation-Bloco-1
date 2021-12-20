4cf4fcef96ef0d645d068cff4d0ce04b0c24beb5
Lucas Oi <lucas.oi-@hotmail.com>
2021-12-20 12:15:50 -0300

    Enviando o Projeto Doe Bhaia

---
+++ ProjetoBloco1/ProjetoDoeBahia/TesteDoador.java
@@ -0,0 +1,15 @@
+package ProjetoDoeBahia;
+
+public class TesteDoador
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		// TODO Auto-generated method stub
+		
+		ProjetoDoacao projeto= new ProjetoDoacao (null,null,null,null,null,null,null,null,null,null,null,0,0);
+		
+		projeto.doadores();
+		//projeto.imprimirInfo();
+	}
+
+}
